d4b88f208ae9d1b47e1c0a3913691dc749ab4a71
miriam <miriamibanezmunoz148@gmail.com>
2025-03-10 00:03:15 +0100

    trasteando multer

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db/db");
 const donutsRouter = require("./src/routes/donutRoutes");  
+const cors = require("cors");
+// app.use("/uploads", express.static("uploads")); //Esto permite acceder a las imágenes desde el frontend usando la URL /uploads/nombreArchivo.jpg.
+
+
 
 dotenv.config();  
 
 const app = express();
-
+app.use(cors());
 
 connectDB();
 
 app.use(express.json());  
 
 
-app.use('/donuts', donutsRouter);
+app.use('/api/donuts', donutsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
--- src/routes/donutRoutes.js
+++ src/routes/donutRoutes.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const router = express.Router();
+
+// const multer = require("multer");
+// const path = require("path");
+
 const {
   getDonuts,
   createDonut,
@@ -7,12 +12,34 @@ const {
   deleteDonut,
 } = require("../controllers/donutsController");
 
-const router = express.Router();
+
 
 router.get("/", getDonuts);
 router.post("/", createDonut);
 router.get("/:id", getDonutById);
 router.put("/:id", updateDonut);
 router.delete("/:id", deleteDonut);
+
+// DE PAGO CON THUNDER
+// Configuración de almacenamiento para las imágenes
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/"); // Carpeta donde se guardarán las imágenes
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname)); // Nombre único
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// // Ruta para subir una imagen
+// router.post("/upload", upload.single("image"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: "No se subió ninguna imagen" });
+//   }
+//   res.json({ imageUrl: `/uploads/${req.file.filename}` }); // Enviar la URL
+// });
+
 
 module.exports = router;
