2c9198f4bdc4c9ae2f0531877b2cd6b4b76431ec
miriam <miriamibanezmunoz148@gmail.com>
2025-03-10 15:27:05 +0100

    retoques

--- src/models/Donut.js
+++ src/models/Donut.js
@@ -5,7 +5,7 @@ const DonutSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  imageUrl: { type: String, required: false }
+  // imageUrl: { type: String, required: false }
 });
 
 module.exports = mongoose.model("Donut", DonutSchema);
